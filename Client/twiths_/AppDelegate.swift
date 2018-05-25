96ae30e3c9792cdbf8e029452a316c76e56a805d
Hong-Sik Kim <>
2018-05-25 16:39:46 +0900

    커밋테스트 가즈아

--- Client/twiths_/AppDelegate.swift
+++ Client/twiths_/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplication {
 
     var window: UIWindow?
 
-
+//ㅁㄴㅇㄹ
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
