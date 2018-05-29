7f3285efc0827f6a078cd1695ddc71e3d2e95c65
Hong-Sik Kim <>
2018-05-29 15:03:46 +0900

    Firebase/Storage를 이용하여 사진 업로드 구현

--- Client/twiths_/Helpers/TourCreateHelper.swift
+++ Client/twiths_/Helpers/TourCreateHelper.swift
@@ -3,12 +3,11 @@
 //  twiths_
 //
 //  Created by yeon suk choi on 2018. 5. 28..
-//  Copyright © 2018년 yeon suk choi. All rights reserved.
+//  Copyright © 2018년 Hanyang University Software Studio 1 TwithS Team. All rights reserved.
 //
 
 import Foundation
 import UIKit
-
 
 func makeBorderToTextField(_ uiTextField:UITextView) {
     uiTextField.layer.borderWidth = 1.0
@@ -18,4 +17,15 @@ func makeBorderToTextField(_ uiTextField:UITextView) {
 
 func makeLimitToMinite(day:Int, hour:Int, min:Int) -> Int {
     return day * 1440 + hour * 60 + min
+}
+
+// 사진 앨범을 띄우는 함수
+func showImgAlbum (imgPicker:UIImagePickerController, TourCreate:TourCreateVC) {
+    if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+        imgPicker.delegate = TourCreate
+        imgPicker.sourceType = .savedPhotosAlbum
+        imgPicker.allowsEditing = false
+        
+        TourCreate.present(imgPicker, animated: true, completion: nil)
+    }
 }
--- Client/twiths_/Tour/TourCreateVC.swift
+++ Client/twiths_/Tour/TourCreateVC.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseStorage
 import FirebaseAuth
 
-class TourCreateVC: UITableViewController {
+class TourCreateVC: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var ImgView1: UIImageView!
+    @IBOutlet var ImgView2: UIImageView!
+    var ImgUrl1:URL! = nil // 투어 이미지의 URL
+    var ImgUrl2:URL! = nil // 랜드마크 이미지의 URL
 
     @IBOutlet weak var TourNameField: UITextField!
     @IBOutlet weak var detailTextField: UITextView!
@@ -20,6 +26,37 @@ class TourCreateVC: UITableViewController {
     @IBOutlet weak var limitDay: UITextField!
     @IBOutlet weak var limitHour: UITextField!
     @IBOutlet weak var limitMin: UITextField!
+    
+    var imgPicker = UIImagePickerController()
+    var IsTourUpload:Bool = true // 투어 이미지 업로드 시 true, 랜드마크 이미지 업로드 시 false
+    
+    // 투어에 있는 이미지 업로드 버튼 터치 시
+    @IBAction func TourImgUpload(_ sender: Any) {
+        IsTourUpload = true
+        showImgAlbum(imgPicker: imgPicker, TourCreate: self)
+    }
+    
+    // 랜드마크 업로드에 있는 이미지 업로드 버튼 터치 시
+    @IBAction func LandmarkImgUpload(_ sender: Any) {
+        IsTourUpload = false
+        showImgAlbum(imgPicker: imgPicker, TourCreate: self)
+    }
+    
+    // 이미지를 선택 완료한 경우
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let url = info[UIImagePickerControllerReferenceURL] as? URL, let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            UserDefaults.standard.set(url, forKey: "assetURL")
+            
+            if IsTourUpload == true {
+                ImgView1.image = image
+                ImgUrl1 = url
+            } else {
+                ImgView2.image = image
+                ImgUrl2 = url
+            }
+        }
+        dismiss(animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,21 +92,51 @@ class TourCreateVC: UITableViewController {
             tour.creator = userID!
             tour.name = TourNameField.text!
             tour.detail = detailTextField.text
+            tour.image = ImgView1.image!.description // 임시로 해당 이미지의 description으로 저장
             tour.timeLimit = makeLimitToMinite(day: Int(limitDay.text!)!, hour: Int(limitHour.text!)!, min: Int(limitMin.text!)!)
             
             tourRef.child("creator").setValue(tour.creator)
             tourRef.child("name").setValue(tour.name)
             tourRef.child("timeLimit").setValue(tour.timeLimit)
             tourRef.child("detail").setValue(tour.detail)
+            tourRef.child("image").setValue(tour.image)
             
             let landmark = Landmark_()
             landmark.tour = String(tourRef.key)
             landmark.name = landmarkNameField.text!
+            landmark.image = ImgView2.image!.description // 임시로 해당 이미지의 description으로 저장
             landmark.detail = landmarkDetailTextField.text
 
             landmarkRef.child("tour").setValue(landmark.tour)
             landmarkRef.child("name").setValue(landmark.name)
             landmarkRef.child("detail").setValue(landmark.detail)
+            landmarkRef.child("image").setValue(landmark.image)
+            
+            // 투어 이미지 올리기
+            let storage1 = Storage.storage()
+            let data1 = UIImagePNGRepresentation(ImgView1.image!)
+            let storRef1 = storage1.reference()
+            let ImgRef1 = storRef1.child(ImgUrl1.path)
+            _ = ImgRef1.putData(data1!, metadata:nil, completion: { (metadata, error) in
+                if let metadata = metadata {
+                    print("Success")
+                } else {
+                    print("Error")
+                }
+            })
+            
+            // 랜드마크 이미지 올리기
+            let storage2 = Storage.storage()
+            let data2 = UIImagePNGRepresentation(ImgView2.image!)
+            let storRef2 = storage2.reference()
+            let ImgRef2 = storRef2.child(ImgUrl2.path)
+            _ = ImgRef2.putData(data2!, metadata:nil, completion: { (metadata, error) in
+                if let metadata = metadata {
+                    print("Success")
+                } else {
+                    print("Error")
+                }
+            })
         }
     }
     
